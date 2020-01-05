b11cc0f8a5c42a757a3c82aa9d034641a7dd248d
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-01-05 19:10:54 +0000

    chore: fix view

--- app/src/main/java/com/adityagupta/foodorderingapp/widget/SquareImageView.kt
+++ app/src/main/java/com/adityagupta/foodorderingapp/widget/SquareImageView.kt
@@ -3,18 +3,16 @@ package com.adityagupta.foodorderingapp.widget
 import android.content.Context
 import androidx.annotation.Nullable
 import android.util.AttributeSet
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 
 
-class SquareImageView : ImageView {
+class SquareImageView : AppCompatImageView {
 
     constructor(context: Context) : super(context) {}
 
     constructor(context: Context, @Nullable attrs: AttributeSet) : super(context, attrs) {}
 
     constructor(context: Context, @Nullable attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
-
-    constructor(context: Context, @Nullable attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {}
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
